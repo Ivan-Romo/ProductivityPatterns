d9626b39b40010705e6636ed206c10668f7ec59a
Ivan-Romo <ivanromontil@gmail.com>
2024-08-25 15:56:13 +0200

    V1 lectura BD

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,20 +49,29 @@ android {
 
 dependencies {
 
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(platform("com.google.firebase:firebase-bom:33.1.2"))
+    implementation(libs.firebase.auth.ktx)
+    implementation(libs.androidx.runtime.livedata)
+    implementation(libs.firebase.firestore.ktx)
+    implementation(libs.androidx.lifecycle.runtime.compose.android)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+
+
+
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.material.icons.extended)
-    implementation(platform("com.google.firebase:firebase-bom:33.1.2"))
-    implementation(libs.firebase.auth.ktx)
-    implementation(libs.androidx.runtime.livedata)
-    // implementation(libs.firebase.auth)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+
+
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
--- app/src/main/java/com/example/productivitypatterns/view/HomeView.kt
+++ app/src/main/java/com/example/productivitypatterns/view/HomeView.kt
@@ -1,5 +1,6 @@
 package com.example.productivitypatterns.view
 
+import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -7,24 +8,37 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.example.productivitypatterns.viewmodel.AuthState
 import com.example.productivitypatterns.viewmodel.AuthViewModel
+import com.example.productivitypatterns.viewmodel.CarListViewModel
 
 @Composable
-fun HomeView(modifier: Modifier = Modifier, navController: NavController, authViewModel: AuthViewModel) {
+fun HomeView(
+    modifier: Modifier = Modifier,
+    navController: NavController,
+    authViewModel: AuthViewModel,
+    carViewModel: CarListViewModel = viewModel()
+) {
     val authState = authViewModel.authState.observeAsState()
 
+    val cars by carViewModel.carList.collectAsStateWithLifecycle()
+
     LaunchedEffect(authState.value) {
-        when(authState.value){
+        when (authState.value) {
             is AuthState.Unauthenticated -> navController.navigate("login")
             else -> Unit
         }
     }
+
+
 
     Column(
         modifier = modifier.fillMaxSize(),
@@ -32,6 +46,13 @@ fun HomeView(modifier: Modifier = Modifier, navController: NavController, authVi
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(text = "Home Page", fontSize = 32.sp)
+
+        for (car in cars) {
+            Text(car.brand + " " + car.id)
+        }
+        
+
+
 
         TextButton(onClick = {
             authViewModel.signout()
