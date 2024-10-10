2283a81a01b902b425cad1fe92c03e1d52c9d23f
Ivan-Romo <ivanromontil@gmail.com>
2024-10-10 13:27:17 +0200

    1.0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.productivity.productivitypatterns"
         minSdk = 26
         targetSdk = 34
-        versionCode = 2
-        versionName = "0.0.2"
+        versionCode = 103
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
